33f9bff4b3039ac2bde159e7bdf64d67b7852c4b
Erfik8 <31968604+Erfik8@users.noreply.github.com>
2019-06-12 20:49:45 +0200

    Revert "Update: added program function"

--- element_listy.cpp
+++ element_listy.cpp
@@ -2,91 +2,42 @@
 #include <windows.h>
 #include "element_listy.h"
 
-void my_new_list::delete_references()
-{
-
-    if (this->previous == NULL && this->next != NULL)
-    {
-        this->next->previous = NULL;
-        this->next = NULL;
-    }
-    else if (this->next == NULL && this->previous != NULL)
-    {
-        this->previous->next = NULL;
-        this->previous = NULL;
-    }
-    else if (this->next != NULL && this->previous != NULL)
-    {
-        this->previous->next = this->next;
-        this->next->previous = this->previous;
-        this->previous = NULL;
-        this->next = NULL;
-    }
-    else
-    {
-        this->next = NULL;
-        this->previous = NULL;
-    }
-}
-
 
 my_new_list::my_new_list()
-    : name("")
-    , surname("")
-    , telephone("")
-    , pesel("")
-    , e_mail("")
-    , ID(0)
-    , previous(NULL)
-    , next(NULL)
+: name("")
+, surname("")
+, telephone("")
+, pesel("")
+, e_mail("")
+, ID(0)
+, previous(NULL)
+, next(NULL)
 {
-    std::string check;
     std::cout << "########################" << std::endl;
     std::cout << "#  WYPELNIJ FORMULARZ  #" << std::endl;
     std::cout << "########################" << std::endl;
 
     std::cout << "Podaj swoje imie:";
-
-    do
-    {
-        std::getline(std::cin,check);
-    }while(check_name_and_surname(check) == false);
-    name = check;
+    std::cin >> name;
 
     std::cout << "Podaj swoje nazwisko:";
-    do
-    {
-        std::getline(std::cin,check);
-    }while(check_name_and_surname(check) == false);
-    surname = check;
+    std::cin >> surname;
 
     std::cout << "Podaj numer telefony:";
-    do
-    {
-        std::getline(std::cin,check);
-    }while(check_telephone_and_pesel(check,9) == false);
-    telephone = check;
+    std::cin >> telephone;
 
     std::cout << "Podaj swoj pesel:";
-    do
-    {
-        std::getline(std::cin,check);
-    }while(check_telephone_and_pesel(check,11) == false);
-    pesel = check;
+    std::cin >> pesel;
 
     std::cout << "Podaj e-mail:";
-    do
-    {
-        std::getline(std::cin,check);
-    }while(check_email(check) == false);
-    e_mail = check;
+    std::cin >> e_mail;
 
 
 
 }
 
 my_new_list::my_new_list(my_new_list *_Previous_, bool next_or_previous)
-    :my_new_list()
+:my_new_list()
 {
     if (next_or_previous == false) // ten element ma sie znalezc za elementem _Previous_
     {
@@ -124,7 +75,24 @@ my_new_list::my_new_list(my_new_list *_Previous_, bool next_or_previous)
 my_new_list::~my_new_list()
 {
     std::cout<<"Usuwanie obiektu"<<name<<std::endl;
-    this->delete_references();
+
+    if (this->previous == NULL)
+    {
+        this->next->previous = NULL;
+        this->next = NULL;
+    }
+    else if (this->next == NULL)
+    {
+        this->previous->next = NULL;
+        this->previous = NULL;
+    }
+    else
+    {
+        this->previous->next = this->next;
+        this->next->previous = this->previous;
+        this->previous = NULL;
+        this->next = NULL;
+    }
 }
 
 void my_new_list::show(bool show_all)
@@ -139,8 +107,7 @@ void my_new_list::show(bool show_all)
     std::cout<<"Adres e-mail: :"<<e_mail<<std::endl;
 
 
-    if (show_all == false)
-        return;
+    if (show_all == false) return;
     if (this->next != NULL)
     {
         this->next->show();
@@ -151,8 +118,6 @@ void my_new_list::show(bool show_all)
 void my_new_list::change()
 {
     char zmiana;
-    std::string check;
-
     system("cls");
     std::cout << "Imie: " << name<<std::endl;
     std::cout << "WCISNIJ 'T' ABY ZMIENINC WARTOSC"<<std::endl;
@@ -161,12 +126,7 @@ void my_new_list::change()
     if (zmiana == 'T' || zmiana == 't')
     {
         std::cout << "wprowadz nowa wartosc: ";
-        do
-        {
-            std::getline(std::cin,check);
-        }
-        while(check_name_and_surname(check) == false);
-        name = check;
+        std::cin >> name;
     }
 
 
@@ -178,12 +138,7 @@ void my_new_list::change()
     if (zmiana == 'T' || zmiana == 't')
     {
         std::cout << "wprowadz nowa wartosc: ";
-        do
-        {
-            std::getline(std::cin,check);
-        }
-        while(check_name_and_surname(check) == false);
-        surname = check;
+        std::cin >> surname;
     }
 
 
@@ -195,12 +150,7 @@ void my_new_list::change()
     if (zmiana == 'T' || zmiana == 't')
     {
         std::cout << "wprowadz nowa wartosc: ";
-        do
-        {
-            std::getline(std::cin,check);
-        }
-        while(check_telephone_and_pesel(check,9) == false);
-        telephone = check;
+        std::cin >> telephone;
     }
 
 
@@ -212,12 +162,7 @@ void my_new_list::change()
     if (zmiana == 'T' || zmiana == 't')
     {
         std::cout << "wprowadz nowa wartosc: ";
-        do
-        {
-            std::getline(std::cin,check);
-        }
-        while(check_telephone_and_pesel(check,11) == false);
-        pesel = check;
+        std::cin >> pesel;
     }
 
 
@@ -229,12 +174,7 @@ void my_new_list::change()
     if (zmiana == 'T' || zmiana == 't')
     {
         std::cout << "wprowadz nowa wartosc: ";
-        do
-        {
-            std::getline(std::cin,check);
-        }
-        while(check_email(check) == false);
-        e_mail = check;
+        std::cin >> e_mail;
     }
     system("cls");
     std::cout<<"czy pokazane dane sie zgadzaja? "<<std::endl<<std::endl;
@@ -250,8 +190,7 @@ void my_new_list::change()
     {
         this->change();
     }
-    else
-        return;
+    else   return;
 
 }
 
@@ -262,150 +201,45 @@ void my_new_list::set_new_ID_number(int number)
     {
         this->next->set_new_ID_number(++number);
     }
-    else
-        return;
+    else return;
 }
 
-bool my_new_list::replace_element(my_new_list *friend_element, bool if_before)
-{
-    if (friend_element == NULL)
-    {
-        std::cout << "FATAL ERROR: OBIEKT NIE ISTNIEJE" << std::endl<< std::endl;
-        std::cout << "ZAMKN¥C PROGRAM, CZY KONTYNUOWAC?" << std::endl;
-        std::cout << "WCISNIJ 'Y' ABY WYLACZYC PROGRAM AWARYJNIE"<< std::endl;
-        std::cout << "WCISNIJ DOWOLNY KLAWISZ ABY KONTYUOWAC" << std::endl;
-        if (getch() == 'y' || getch() == 'Y')
-            return false;
-        else
-            return true;
-    }
-    else
-    {
-        this->delete_references();
-        if (if_before == true) // in this case, replaced element will be put before friend_element
-        {
-            if (friend_element->previous == NULL && this->next == NULL)
-            {
-                friend_element->previous = this;
-                this->next = friend_element;
-            }
-            else if (this->next == NULL)
-            {
-                friend_element->previous->next = this;
-                this->previous = friend_element->previous;
-                this->next = friend_element;
-                friend_element->previous = this;
-            }
-            else
-            {
-                std::cout << "ERROR: niepowodzenie usuwania powiazan, element nie moze byc przeniesiony"<<std::endl;
-                return true;
-            }
-        }
-        else
-        {
-            if (friend_element->next == NULL && this->previous == NULL)
-            {
-                friend_element->next = this;
-                this->previous = friend_element;
 
-            }
-            else if (this->previous == NULL)
-            {
-                friend_element->next->previous = this;
-                this->next = friend_element->next;
-                this->previous = friend_element;
-                friend_element->next = this;
-            }
-            else
-            {
-                std::cout << "ERROR: niepowodzenie usuwania powiazan, element nie moze byc przeniesiony"<<std::endl;
-                return true;
-            }
-        }
-    }
-    return true;
-}
 
-void my_new_list::DELETE_ALL_LIST(bool direction, bool first_call)
+void Show_Manage_menu()
 {
-    if (first_call == true)
-    {
-        if (this->next != NULL)
-            this->next->DELETE_ALL_LIST(true, false);
-        if (this->previous != NULL)
-            this->previous->DELETE_ALL_LIST(false, false);
-    }
-    else if (direction == true) // in this case, function will find elements by 'next' pointer
-    {
-        if (this->next != NULL)
-            this->next->DELETE_ALL_LIST(true, false);
-    }
-    else
-    {
-        if (this->previous != NULL)
-            this->previous->DELETE_ALL_LIST(false, false);
-    }
-    delete this;
+    std::cout << "wybierz jedna z opcji, wciskajac numer na klawiaturze"<<std::endl
+                 << "-----------------------------------------------------"<<std::endl
+                 << "1. dodaj element przed aktualny"<<std::endl
+                 << "2. dodaj element po aktualnym"<<std::endl
+                 << "3. usun aktualny element"<<std::endl
+                 << "4. przesun dany element"<<std::endl
+                 << "5. zmien parametr danego elementu"<<std::endl
+                 << "6. szukaj elementu, wybierajac parametr oraz wpisujac fraze"<<std::endl;
+            std::cout << "wcisnij dana litere, aby przeniesc sie do innego elementu"<<std::endl
+                 << "---------------------------------------------------------"<<std::endl
+                 << "A. element wczesniej"<<std::endl
+                 << "D. element dalej"<<std::endl
+                 << "J. skocz do innego elemntu, wpisujac jego numer"<<std::endl
+                 << "S. idz na poczatek"<<std::endl
+                 << "F. idz na koniec"<<std::endl
+                 << "wcisnij dowolny klawisz, aby wyjsc"<<std::endl;
 }
 
 my_new_list *my_new_list::get_element_by_ID(int const id)
 {
     if (this->ID < id)
     {
-        if (this->next == NULL)
-            return NULL;
+        if (this->next == NULL) return NULL;
 
-        return this->next->get_element_by_ID(id);
+        this->next->get_element_by_ID(id);
     }
     else if (this->ID > id)
     {
-        if (this->previous == NULL)
-            return NULL;
+        if (this->previous == NULL) return NULL;
 
-        return this->previous->get_element_by_ID(id);
+        this->previous->get_element_by_ID(id);
     }
-    else if (this->ID == id)
-        return this;
-    else
-        return NULL;
-}
-
-my_new_list *my_new_list::get_next_element(my_new_list * const First, my_new_list * const Last)
-{
-    my_new_list *Next_element = NULL;
-    if (this->next == NULL && this == Last)
-    {
-        Next_element = First;
-    }
-    else if (this->next != NULL)
-    {
-        Next_element = this->next;
-    }
-    else
-    {
-        std::cout << "FATAL ERROR: BLAD ZAPISU OBIEKTOW, WYLACZENIE PROGRAMU";
-        this->DELETE_ALL_LIST();
-        exit(0);
-    }
-    return Next_element;
-}
-my_new_list *my_new_list::get_previous_element(my_new_list *const First,my_new_list *const Last)
-{
-    my_new_list *Previous_element = NULL;
-    if (this->previous == NULL && this == First)
-    {
-        Previous_element = Last;
-    }
-    else if (this->previous != NULL)
-    {
-        Previous_element = this->previous;
-    }
-    else
-    {
-        std::cout << "FATAL ERROR: BLAD ZAPISU OBIEKTOW, WYLACZENIE PROGRAMU";
-        this->DELETE_ALL_LIST();
-        exit(0);
-    }
-    return Previous_element;
+    else if (this->ID == id) return this;
+    else return NULL;
 }
--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 #include <windows.h>
 #include "element_listy.h"
 #include <conio.h>
-//#include <cstdlib>
 
 int main()
 {
 
+    my_new_list *New_list = NULL;
     my_new_list *_First_ = NULL;
     my_new_list *_Last_ = NULL;
     for(;;)
@@ -29,7 +29,7 @@ int main()
         {
         case '1':
         {
-            if (_First_ == NULL)
+            if (New_list == NULL)
             {
                 std::cout << "LISTA JEST PUSTA, CHCESZ STWORZYC NOWA?" << std::endl;
                 std::cout << "WCISNIJ 'T' ABY STWORZYC NOWA LISTE    " << std::endl;
@@ -39,8 +39,9 @@ int main()
                 case 'T':
                 case 't':
                     {
-                        _First_ = new my_new_list;
-                        _Last_ = _First_;
+                         New_list = new my_new_list;
+                        _First_ = New_list;
+                        _Last_ = New_list;
                     }break;
                 default:    Sleep(2000);    break;
                 }
@@ -111,131 +112,6 @@ int main()
                                 _First_->set_new_ID_number(0);
 
                             }break;
-                        case '4':
-                            {
-                                my_new_list *Replace = Management;
-                                bool accepted = false;
-
-                                do
-                                {
-                                    std::string search_element = "";
-                                    system("cls");
-                                    Replace->show(false);
-                                    std::cout << std::endl;
-
-                                    std::cout << "wpisz liczbe, aby przeskoczyc do elementu po ID" << std::endl;
-                                    std::cout << "wpisz '/SEARCH' aby znalezc element po odpowiednim parametrze" <<  std::endl;
-                                    std::cout << "wcisnij i zatwierdz 'A' lub 'D', aby pourszac sie po kolejnych elementach bazy" << std::endl;
-                                    std::cout << "wcisnij i zatwierdz 'C' aby anulowac przenoszenie" << std::endl;
-                                    std::cout << "wcisnij enter, aby zaakceptowac" << std::endl;
-                                    std::getline(std::cin,search_element);
-
-                                    if (search_element == "/SEARCH")
-                                    {
-                                        // tu metoda do napisania szukania elementu po frazie
-                                    }
-                                    else if (search_element == "A" || search_element == "a")
-                                    {
-                                        Replace = Replace->get_previous_element(_First_,_Last_);
-                                    }
-                                    else if (search_element == "D" || search_element == "d")
-                                    {
-                                        Replace = Replace->get_next_element(_First_,_Last_);
-                                    }
-                                    else if (search_element == "C" || search_element == "c")
-                                    {
-                                        Replace = NULL;
-                                        break;
-                                    }
-                                    else if (search_element == "0")
-                                    {
-                                        Replace = _First_;
-                                    }
-                                    else if (atoi(search_element.c_str()) != 0)
-                                    {
-
-                                        int return_id = atoi(search_element.c_str());
-                                        Replace = Replace->get_element_by_ID(return_id);
-                                        if (Replace == NULL)
-                                        {
-                                            std::cout << "ERROR: nie ma takiego elementu" << std::endl;
-                                            Sleep(2000);
-                                            Replace = Management;
-                                        }
-                                    }
-                                    else if (search_element == "")
-                                    {
-                                        if (Management == Replace)
-                                        {
-                                            std::cout << "ERROR: wybrano ten sam element, ktory ma byc przeniesiony, powrot do menu wyboru" << std::endl;
-                                            Sleep(1500);
-                                        }
-                                        else
-                                        {
-                                            bool all_OK =true;
-                                            std::cout << std::endl;
-                                            std::cout << "B - wstaw przed wybranym elementem"<< std::endl;
-                                            std::cout << "A - wstaw za wybranym elementem"<< std::endl;
-                                            std::cout << "Wcisnij dowolny klawisz, aby anulowac"<< std::endl;
-                                            switch (getch())
-                                            {
-                                            case 'A':
-                                            case 'a':
-                                                {
-                                                    if (Replace == _Last_)
-                                                    {
-                                                        _Last_ = Management;
-                                                    }
-                                                    if (Management == _First_)
-                                                    {
-                                                        _First_ = Management->next;
-                                                    }
-                                                    all_OK = Management->replace_element(Replace,false);
-
-                                                }break;
-                                            case 'B':
-                                            case 'b':
-                                                {
-                                                    if (Replace == _First_)
-                                                    {
-                                                        _First_ = Management;
-                                                    }
-                                                    if (Management == _Last_)
-                                                    {
-                                                        _Last_ = Management->previous;
-                                                    }
-                                                    all_OK = Management->replace_element(Replace,true);
-                                                }
-                                            default: break;
-                                            }
-                                            if (all_OK == false)
-                                            {
-                                                std::cout << "FATAL ERROR: AWARYJNE WYLACZENIE PROGRAMU" << std::endl;
-                                                Management = NULL;
-                                                Replace = NULL;
-                                                _First_->DELETE_ALL_LIST();
-                                                if (_First_ != NULL) delete _First_;
-                                                _First_ = NULL;
-                                                _Last_ = NULL;
-                                                exit(0);
-                                            }
-                                            accepted = true;
-                                        }
-                                    }
-                                    else
-                                    {
-                                        std::cout << "wprowadzono nieprawidlowe dane, wpisz jeszcze raz" << std::endl;
-                                        Sleep(1500);
-                                    }
-
-                                }while(accepted != true);
-
-                                std::cout << "Operacja zakonczona powodzeniem" << std::endl;
-                                Sleep(2000);
-                                system("cls");
-                                Management = _First_;
-                                Management->set_new_ID_number(0);
-                            }break;
                         case '5':
                             {
                                 Management->change();
@@ -244,13 +120,37 @@ int main()
                         case 'D':
                         case 'd':
                             {
-                                Management = Management->get_next_element(_First_, _Last_);
+                                if (Management->next == NULL && Management == _Last_)
+                                {
+                                    Management = _First_;
+                                }
+                                else if (Management->next != NULL)
+                                {
+                                    Management = Management->next;
+                                }
+                                else
+                                {
+                                    std::cout << "FATAL ERROR: BLAD ZAPISU OBIEKTOW, WYLACZENIE PROGRAMU";
+                                    exit(0);
+                                }
                             }break;
 
                         case 'A':
                         case 'a':
                             {
-                                Management = Management->get_previous_element(_First_, _Last_);
+                                if (Management->previous == NULL && Management == _First_)
+                                {
+                                    Management = _Last_;
+                                }
+                                else if (Management->previous != NULL)
+                                {
+                                    Management = Management->previous;
+                                }
+                                else
+                                {
+                                    std::cout << "FATAL ERROR: BLAD ZAPISU OBIEKTOW, WYLACZENIE PROGRAMU";
+                                    exit(0);
+                                }
                             }break;
                         case 'J':
                         case 'j':
@@ -280,7 +180,7 @@ int main()
         case '2':
         {
 
-            if (_First_ == NULL)
+            if (New_list == NULL)
             {
                 std::cout << "# NIE STWORZONO ZADNEJ LISTY #" <<std::endl;
                 Sleep(2000);
@@ -304,12 +204,7 @@ int main()
                       << "PRZED ZAMKNIECIEM PROGRAMU ZAPISZ LISTE, JESLI JESZCZE TEGO"<<std::endl
                       << "NIE ZROBILES. CZY MIMO TO CHCESZ ZAMKNAC PROGRAM? <Y OR N>"<<std::endl;
             if (getch()=='y'||getch()=='Y')
-            {
-                _First_->DELETE_ALL_LIST();
-                _First_ = NULL;
-                _Last_ = NULL;
                 exit(0);
-            }
         }
         break;
         case '6':
